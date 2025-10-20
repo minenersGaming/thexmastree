150b111019629bedca877aace34dac2f4cfcd126
mineners <76274288+minenersGaming@users.noreply.github.com>
2025-10-20 16:48:39 +0700

    copy bar update

--- frontend/xmas-app/src/components/CopyLinkBar.tsx
+++ frontend/xmas-app/src/components/CopyLinkBar.tsx
@@ -1,21 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CopyLinkBar = () => {
+    const textToCopy = 'https://xmas.tucm.cc/t/ilovetucmc'; //replace with API data
+    const [copyStatus, setCopyStatus] = useState('');
+
+    const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      setCopyStatus('success');
+      setPopUp(true);
+      setTimeout(() => setCopyStatus(''), 2000); // Clear message after 2 seconds
+    } catch (err) {
+      setCopyStatus('failed');
+    }
+  };
+
     const [isExtend, setExtend] = useState(false);
     const [isPopUp, setPopUp] = useState(false);
     function UpdateBar() {
         !isExtend && setExtend(true);
     };
 
+    useEffect(() => {
+
+       function resetBlob() {
+        setPopUp(false);
+        clearTimeout(timer);
+    } 
+
+    if (isPopUp) {
+      const timer = setTimeout(() => {
+      }, 3000);
+      
+      return () => resetBlob;
+    } else {
+    }
+
+    
+
+  }, [isPopUp]);
+
     return <>
     <div className="flex w-full justify-center">
+        <div className={`text-[13px] font-inter bottom-[100px] text-white absolute z-[800] px-3 py-2 rounded-full 
+        ${isPopUp ? "anim-intro-easeIn" : ""}`} 
+        style={{
+                backgroundColor: "rgba(0, 0, 0, 0.50)",
+                backdropFilter: "blur(2px)",
+                visibility: (isPopUp ? "visible" : "hidden"),
+                }}>Link Copied!</div>
         <div onClick={UpdateBar} className="transition-all bottom-[40px] absolute grad-commonred z-[900] text-white font-bold p-3 text-center rounded-full" style={{
-            width: (!isExtend ? "200px" : "290px"),
+            width: (!isExtend ? "200px" : "320px"),
             fontSize: (!isExtend ? "20px" : "15px"),
         }}>
             <div className="flex flex-row justify-evenly">
-                <p>{ !isExtend ? "Copy Link" : "https://tucm.cc/t/ilovetucmc" }</p> {/** FIX THE LINK LATER */}
-                <img className="w-[20px]" src="/src/assets/copyTab.svg"></img></div>
+                <p>{ !isExtend ? "Copy Link" : "https://xmas.tucm.cc/t/ilovetucmc" }</p> {/** FIX THE LINK LATER */}
+                <button onClick={handleCopy}><img className="w-[20px]" src="/src/assets/copyTab.svg"></img></button>
+                </div>
             </div>
     </div>
     </>;
